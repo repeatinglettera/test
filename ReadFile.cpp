e6a304d48b6d49b6ef319153eeea5e86ce9856be
Amanda Estep <arestep42@students.tntech.edu>
2017-01-30 19:42:38 -0600

    Removed comments from readfile

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -2,16 +2,6 @@
 #include <iostream>
 #include <string>
 
-/*ReadFile* createReadFile(const char* file_name)
-{
-   ReadFile* rf = new ReadFile;
-
-   rf->input_file.open(file_name);
-   rf->closed = false;
-   rf->_eof = false;
-
-   return rf;
-}*/
 ReadFile::ReadFile(const char* file_name)
 {
    input_file.open(file_name);
@@ -19,33 +9,16 @@ ReadFile::ReadFile(const char* file_name)
    _eof = false;
 }	
 
-/*void destroyReadFile(ReadFile* rf)
-{
-   close(rf);
-   delete rf;
-}*/
 ReadFile::~ReadFile()
 {
    close();
 }
 
-/*bool eof(ReadFile* rf)
-{
-   return rf->_eof;
-}*/
 bool ReadFile::eof()
 {
    return _eof;
 }
 
-/*void close(ReadFile* rf)
-{
-   if (!rf->closed)
-   {
-      rf->input_file.close();
-      rf->closed = true;
-   }
-}*/
 void ReadFile::close()
 {
    if (!closed)
@@ -55,17 +28,6 @@ void ReadFile::close()
    }
 }
 
-/*String* readLine(ReadFile* rf)
-{
-   if (rf->closed) return NULL;
-   if (rf->_eof) return NULL;
-
-   string text;
-   rf->_eof = !(getline(rf->input_file, text));
-
-   String* str = new String((const char*) text.c_str());
-   return str;
-}*/
 String* ReadFile::readLine()
 {
    if (closed) return NULL;
